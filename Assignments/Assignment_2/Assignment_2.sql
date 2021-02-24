c45652e13d14e4cfaafc63b55256b25a166497f9
MeghanLAnderson <s01136176@acad.tri-c.edu>
2021-02-24 17:38:32 -0500

    Complete through G

--- Assignments/Assignment_2/Assignment_2.sql
+++ Assignments/Assignment_2/Assignment_2.sql
@@ -43,8 +43,15 @@ ON o.order_id = od.order_id
 ORDER BY i.artist
 ;
 
---Once again you will add to the last query replacing --customer_id with customer name.  Add another join that --will accomplish this goal. Name the image and script --files: Assignment2Part2E
-SELECT o.customer_id, o.order_date, i.artist, od.order_qty, c.customer_first_name
+--Once again you will add to the last query replacing 
+--customer_id with customer name.  Add another join that 
+--will accomplish this goal. Name the image and script 
+--files: Assignment2Part2E
+SELECT CONCAT(c.customer_first_name,' ',c.customer_last_name) as name
+      , o.order_date
+      , i.artist
+      , od.order_qty
+      --, c.customer_first_name
 FROM orders o 
   INNER JOIN order_details od
   INNER JOIN items i
@@ -55,21 +62,36 @@ ON o.order_id = od.order_id
 ORDER BY c.customer_first_name
 ;
 
---Output allthe titles with an order quantity over one. --Name the image and script files: Assignment2Part2F. 
+--Output allthe titles with an order quantity over one. 
+--Name the image and script files: Assignment2Part2F. 
 SELECT i.title, od.order_qty
 FROM items i
-  INNER JOIN order_details od
+  JOIN order_details od
 ON i.item_id = od.item_id
 WHERE order_qty > 1
 ;
---Output allthe orders for customers in Ohio. Include the customer --names and titles purchasedin your output. Name your image and -script files:Assignment2Part2G
-SELECT concat(customer_first_name, " ", customer_last_name) as Name, customer_state, title
-FROM customers, items
-WHERE customer_state = 'OH'
+
+-- Output all the orders for customers in Ohio. 
+-- Include the customer names and titles purchased in your output. 
+-- Name your image and script files:
+-- Assignment2Part2G
+-- Output a list of Customers and orders including titles for customers in ohio
+
+SELECT CONCAT(c.customer_first_name,' ',c.customer_last_name) as name
+        , o.order_id
+        , i.title
+      --  , c.customer_state
+FROM customers c
+    JOIN orders o
+    JOIN order_details od
+    JOIN items i
+WHERE c.customer_id = o.customer_id
+    AND o.order_id = od.order_id
+    AND od.item_id = i.item_id
+    AND c.customer_state ='OH'
 ;
 
---Insert a row into the customers table(data of your choice). --Nameyour image and script filesAssignment2Part2
+--Insert a row into the customers table(data of your choice).
+--Nameyour image and script filesAssignment2Part2H
 SELECT
 FROM
-
-
