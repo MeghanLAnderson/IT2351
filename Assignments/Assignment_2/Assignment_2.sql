023d89617b08deb2dc294dc927cc5693ebd05886
MeghanLAnderson <s01136176@acad.tri-c.edu>
2021-02-25 12:23:50 -0500

    update comments

--- Assignments/Assignment_2/Assignment_2.sql
+++ Assignments/Assignment_2/Assignment_2.sql
@@ -4,9 +4,8 @@
 -- sort your output by title and then artist. Name your imageand 
 -- script file IT2351Assignment2Part2A.  
 
-
---IT2351Assignment2Part2A
 -- Select data from two tables joined by common key item_id
+-- IT2351Assignment2Part2A
 SELECT  title, artist, unit_price, order_qty
 FROM items i INNER JOIN order_details od
 ON i.item_id = od.item_id
@@ -15,7 +14,8 @@ ON i.item_id = od.item_id
 --Assignment2Part2B
 -- Output customername (concatenated), city, state, order_date and ship_date for each order.  
 -- Sort the output by state, city, last name, first name. 
--- Name your image and script files: Assignment2Part2B. 
+-- Name your image and script files: 
+-- Assignment2Part2B. 
 SELECT concat(customer_first_name, " ", customer_last_name) as Name,c.customer_city,c.customer_state,o.order_date,o.shipped_date
 FROM customers c JOIN orders o
 ON c.customer_id = o.customer_id
@@ -25,15 +25,19 @@ ORDER BY c.customer_state, c.customer_city, c.customer_last_name, c.customer_fir
 --  Assignment2Part2C
 -- Output the detail for each order, inother words show 
 -- the customer_id, order_date, item_id and order_qty for each order. 
--- Name the image and script files: Assignment2Part2C.
+-- Name the image and script files: 
+-- Assignment2Part2C.
 SELECT customer_id, order_date, item_id, order_qty
 FROM orders o INNER JOIN order_details od
 ON o.order_id = od.order_id
 ORDER BY o.customer_id
 ;
 
--- QUERY D 
--- READ AND UNDERSTAND
+-- Looking at your last query, it would be helpful to have item names (title) rather than item_id. 
+-- Update the last query to replace item_id with artist (add another join). 
+-- Name the image and script files:
+-- Assignment2Part2D.
+script files:Assignment2Part2D
 SELECT o.customer_id, o.order_date, i.artist, od.order_qty
 FROM orders o 
   INNER JOIN order_details od
@@ -46,7 +50,7 @@ ORDER BY i.artist
 --Once again you will add to the last query replacing 
 --customer_id with customer name.  Add another join that 
 --will accomplish this goal. Name the image and script 
---files: Assignment2Part2E
+-- Assignment2Part2E
 SELECT CONCAT(c.customer_first_name,' ',c.customer_last_name) as name
       , o.order_date
       , i.artist
@@ -63,7 +67,8 @@ ORDER BY c.customer_first_name
 ;
 
 --Output allthe titles with an order quantity over one. 
---Name the image and script files: Assignment2Part2F. 
+--Name the image and script files: 
+-- Assignment2Part2F. 
 SELECT i.title, od.order_qty
 FROM items i
   JOIN order_details od
@@ -75,7 +80,6 @@ WHERE order_qty > 1
 -- Include the customer names and titles purchased in your output. 
 -- Name your image and script files:
 -- Assignment2Part2G
--- Output a list of Customers and orders including titles for customers in ohio
 
 SELECT CONCAT(c.customer_first_name,' ',c.customer_last_name) as name
         , o.order_id
@@ -92,6 +96,7 @@ WHERE c.customer_id = o.customer_id
 ;
 
 --Insert a row into the customers table(data of your choice).
---Nameyour image and script filesAssignment2Part2H
+--Name your image and script files
+-- Assignment2Part2H
 SELECT
 FROM
