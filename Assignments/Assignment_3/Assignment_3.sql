a84d8ff0ab38f7d111b5d3afc1b6ef929819dc4e
mla655 <mla655@CP4F7Y2>
2021-03-12 14:03:13 -0500

    polished proj

--- Assignments/Assignment_3/Assignment_3.sql
+++ Assignments/Assignment_3/Assignment_3.sql
@@ -1,8 +1,12 @@
--- Assignment3No2
+-- The count of the number of orders in the orders table
+-- The sum of the “Ship Value” which is the sum of the ship_amount from the orders table.
 SELECT COUNT(order_id), SUM(ship_amount) as "Ship Value"
 FROM orders
 ;
--- 3.
+-- The Category Name column from the Categories table
+-- b) The count of the products in the Products table (join these two tables)
+-- c) The price of the most expensive product in the Products table
+-- d) Sort the result set so the category with the most products appears first.
 SELECT c.category_name, COUNT(product_id), MAX(list_price)
 FROM categories c
     JOIN products p
@@ -10,7 +14,13 @@ ON c.category_id = p.category_id
 GROUP BY c.category_name
 ORDER BY COUNT(product_id)
 ;
--- 4.
+-- The CategoryName column from the category table
+-- b) The ProductName column from the products table
+-- c) The total quantity purchased for each product with orders in the OrderDetails table
+-- d)Use the WITH ROLLUP operator to include rows that give a summary for each category name as well
+-- as a row that gives the grand total. Use the IF and GROUPING functions to replace null values in the
+-- category_name and product_name columns with literal values if they’re for summary rows.
+
 SELECT 
     IF (GROUPING (c.category_name), 'All Categories', c.category_name) as category_name,
     IF (GROUPING (p.product_name), 'All Products' , p.product_name) as product_name,
@@ -22,7 +32,8 @@ ON c.category_id = p.category_id
     AND p.product_id = i.product_id
 GROUP BY c.category_name, p.product_name WITH ROLLUP
 ;
--- 5
+--  Write a SELECT statement that returns the same result set as this SELECT statement, but don’t use a
+-- join.
 SELECT DISTINCT category_name
 FROM categories c
 WHERE category_id IN (SELECT category_id FROM products)
