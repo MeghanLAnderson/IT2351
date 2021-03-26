6e987ed512932138b1f4d29497a3ce990e5e1e2f
MeghanLAnderson <s01136176@acad.tri-c.edu>
2021-03-26 14:28:12 -0400

    push assignment 4

--- Assignments/Assignment_4/Assignment_4.sql
+++ Assignments/Assignment_4/Assignment_4.sql
@@ -7,9 +7,9 @@ FROM products
 -- Assignment4No2
 SELECT email_address,
        length(email_address) as length,
-       locate('@', email_address) as @,
-       LEFT(email_address, 8) as username,
-       RIGHT(email_address, 8) as domain
+       locate('@', email_address) as "@",
+       LEFT(email_address, locate('@',email_address) - 1) as username,
+       SUBSTRING(email_address, locate('@',email_address) + 1) as domain
 FROM customers 
 ;
 -- Assignment4No3
@@ -19,20 +19,20 @@ FROM products
 ;
 -- Assignment4No4
 SELECT order_id, order_date, ship_date,
-       DATE_ADD(order_date, INTERVAL 2 DAYS) as approx_ship_date,
+       DATE_ADD(order_date, INTERVAL 2 DAY) as approx_ship_date,
        DATEDIFF(order_date,ship_date) as days_to_ship
 FROM orders
-WHERE start_date = '2018-03-01'
+WHERE EXTRACT(YEAR_MONTH FROM order_date) ="201803"
 GROUP BY order_id, order_date, ship_date
 ;
 -- Assignment4No5
 SELECT c.category_name, 
        p.product_name,
-       SUM(o.item_price) as total_sales
-       FIRST VALUE(o.category_id) OVER (
+       SUM(o.item_price) as total_sales,
+       FIRST_VALUE(c.category_name) OVER (
            PARTITION BY o.item_price order by o.order_id DESC
        ) as highest_sales,
-        LAST VALUE(o.category_id) OVER (
+        LAST_VALUE(c.category_name) OVER (
            PARTITION BY o.item_price order by o.order_id DESC
            RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
        ) as lowest_sales
@@ -43,6 +43,28 @@ ON c.category_id = p.category_id
     AND p.product_id = o.product_id
 GROUP BY c.category_name, p.product_name
 ;
+
+
+SELECT c.category_name, 
+       p.product_name,
+       SUM(o.item_price) as total_sales,
+       FIRST_VALUE(p.product_name) OVER (
+        PARTITION BY c.category_name order by SUM(o.item_price) DESC
+       ) as highest_sales,
+       FIRST_VALUE(p.product_name) OVER (
+        PARTITION BY c.category_name order by SUM(o.item_price) DESC
+        RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+       ) as lowest_sales
+FROM categories c
+     JOIN products p
+     JOIN order_items o
+ON c.category_id = p.category_id
+    AND p.product_id = o.product_id
+GROUP BY c.category_name, p.product_name,o.item_price,o.order_id
+;
+
+
+
 -- Assignment4No6
 CREATE VIEW order_item_products AS
 SELECT o.order_id, o.order_date, o.tax_amount, o.ship_date, p.product_name, i.item_price, i.discount_amount,
