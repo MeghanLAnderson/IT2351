0497eaac47c917e4c10a7c9bc24a39e0cb5c0617
MeghanLAnderson <s01136176@acad.tri-c.edu>
2021-04-08 13:12:43 -0400

    add assignment_5

---
+++ Assignments/Assignment_5/Lesson5.sql
@@ -0,0 +1,7 @@
+/********************************************************
+* This script creates the database named my_guitar_shop 
+*********************************************************/
+
+DROP DATABASE IF EXISTS MeghanAnderson_lesson5;
+CREATE DATABASE MeghanAnderson_lesson5;
+USE MeghanAnderson_lesson5;
