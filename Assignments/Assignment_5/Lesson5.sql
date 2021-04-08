7ba01d1420581280bb6c0810156c8515725b788c
mla655 <mla655@CP4F7Y2>
2021-04-08 14:07:41 -0400

    work on assignment 5

--- Assignments/Assignment_5/Lesson5.sql
+++ Assignments/Assignment_5/Lesson5.sql
@@ -10,3 +10,22 @@ CREATE TABLE majors (
   major_id        INT            PRIMARY KEY   AUTO_INCREMENT,
   major_name      VARCHAR(255)   NOT NULL      UNIQUE
 );
+
+CREATE TABLE classes (
+  class_id        INT            PRIMARY KEY   AUTO_INCREMENT,
+  class_name      VARCHAR(255)   NOT NULL      UNIQUE
+);
+CREATE TABLE emails (
+  email_id        INT            PRIMARY KEY   AUTO_INCREMENT,
+  email           VARCHAR(255)   NOT NULL      UNIQUE
+  student_id      INT   
+  CONSTRAINT email_fk_students
+    FOREIGN KEY (student_id)
+    REFERENCES students (student_id)
+);
+CREATE TABLE students (
+  student_id        INT            PRIMARY KEY   AUTO_INCREMENT,
+  student_first_name
+  student_last_name
+       VARCHAR(255)   NOT NULL      UNIQUE
+);
